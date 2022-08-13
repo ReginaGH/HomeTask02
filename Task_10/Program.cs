59cd98782004c169766b2f8bcb485d4939658d76
Regina <regina.delmukhametova@gmail.com>
2022-08-13 19:35:20 +0500

    Added task 10

---
+++ Task_10/Program.cs
@@ -0,0 +1,8 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число 
+// и на выходе показывает вторую цифру этого числа.
+Console.WriteLine("Введите трехзачное число");
+int number = Convert.ToInt32(Console.ReadLine());
+int digit1 = number/100;
+int digit2= number%10;
+int digit3=(number-(digit1*100+digit2))/10;
+Console.WriteLine($"Вторая цифра числа: {digit3}");
