06366275005359f5fa81633084b062a02507a0f2
Regina <regina.delmukhametova@gmail.com>
2022-08-14 23:18:18 +0500

    Добавила Task 13. Решение с исп string

---
+++ Task_13_String/Program.cs
@@ -0,0 +1,9 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа 
+// или сообщает, что третьей цифры нет (645 -> 5; 78 -> третьей цифры нет; 32679 -> 6).
+Console.WriteLine("Напишите число ");
+string s = Convert.ToString(Console.ReadLine());
+int third = int.Parse(s[2].ToString());
+if(s.Length>=3)
+    Console.WriteLine(third);
+else if(s.Length<=2)
+    Console.WriteLine("Третьей цифры нет");
