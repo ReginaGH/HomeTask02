996ece46b00c281021d93dd37a50675714ddba7b
Regina <regina.delmukhametova@gmail.com>
2022-08-14 21:05:05 +0500

    Добавила условие отриц числа

--- Task_10/Program.cs
+++ Task_10/Program.cs
@@ -4,5 +4,13 @@ Console.WriteLine("Введите трехзачное число");
 int number = Convert.ToInt32(Console.ReadLine());
 int digit1 = number/100;
 int digit2= number%10;
-int digit3=(number-(digit1*100+digit2))/10;
-Console.WriteLine($"Вторая цифра числа: {digit3}");
+if(number<0)
+{
+    int digit3=(number*(-1)-(digit1*(-100)+digit2*(-1)))/10;
+    Console.WriteLine($"Вторая цифра числа: {digit3}");
+}
+else
+{
+    int digit3=(number-(digit1*100+digit2))/10;
+    Console.WriteLine($"Вторая цифра числа: {digit3}");
+}
