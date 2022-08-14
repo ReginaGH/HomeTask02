f71750842b012bc0fe696107d549361ddfc67695
Regina <regina.delmukhametova@gmail.com>
2022-08-14 21:07:38 +0500

    Решение задачи 10 через текст

---
+++ Task_10_OtherWay/Program.cs
@@ -0,0 +1,6 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число 
+// и на выходе показывает вторую цифру этого числа.
+Console.WriteLine("Введите трехзачное число");
+string s = Convert.ToString(Console.ReadLine());
+int second = int.Parse(s[s.Length-2].ToString());
+Console.WriteLine(second);
