f017e87d4c0f66ba8d49d4ab118c502c25f029a1
Regina <regina.delmukhametova@gmail.com>
2022-08-15 18:28:27 +0500

    Добавила модуль числа в Task 10 OtherWay

--- Task_10_OtherWay/Program.cs
+++ Task_10_OtherWay/Program.cs
@@ -1,6 +1,11 @@
 ﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число 
 // и на выходе показывает вторую цифру этого числа.
 Console.WriteLine("Введите трехзачное число");
-string s = Convert.ToString(Console.ReadLine());
+//string s = Convert.ToString(Console.ReadLine());
+//int second = int.Parse(s[s.Length-2].ToString());
+//Console.WriteLine(second);
+int number = Convert.ToInt32(Console.ReadLine());
+number = Math.Abs(number);
+string s = Convert.ToString(number);
 int second = int.Parse(s[s.Length-2].ToString());
 Console.WriteLine(second);
